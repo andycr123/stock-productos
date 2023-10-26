94dc7f028526885d76ed983ca9fa7fcb41acc058
andycr123 <81699558+andycr123@users.noreply.github.com>
2023-10-26 15:42:38 -0500

    changes

--- stock-qr/src/App.js
+++ stock-qr/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px-2">
      <h1>Hello</h1>
     </div>
   );

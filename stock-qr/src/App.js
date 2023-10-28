d686d847ec72f0531b4bc0ca9c3911b4659dbca2
andycr123 <81699558+andycr123@users.noreply.github.com>
2023-10-27 20:12:43 -0500

    changes

--- stock-qr/src/App.js
+++ stock-qr/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px-2">
+    <div className="bg-white shadow rounded">
      <h1>Hello</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,15 @@ module.exports = {
   plugins: [],
 }
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // ...
+}
